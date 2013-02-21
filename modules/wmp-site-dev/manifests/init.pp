2b185216468fed8960b3dda09e85a0fadff2c095
root <root@puppet.aws.internal>
2013-02-21 18:19:08 +0000

    Dev version of wmp code module which installs svn

---
+++ modules/wmp-site-dev/manifests/init.pp
@@ -0,0 +1,36 @@
+# == Class: wmp-site
+class wmp-site-dev ( $mysql = undef ) {
+  include wmp-dirs
+
+  package { 'subversion':
+    ensure  => installed,
+  } 
+
+  package { 'cup-wmp-site':
+    ensure  => installed,
+    require => Package['httpd'],
+  } ~>
+         
+  file { 'data-logs-httpd-wmp-dir':
+    path    => '/data/logs/httpd/wmp',
+    ensure  => directory,
+    owner   => 'apache',
+    group   => 'apache',
+    mode    => '0755',
+  } ~>
+
+  file { 'data-httpd-wmp-tmp-dir':
+    path    => '/data/httpd/wmp/tmp',
+    ensure  => directory,
+    owner   => 'apache',
+    group   => 'apache',
+    mode    => '0755',
+  } ~>
+
+  file { 'wmp.conf':
+    path    => '/etc/httpd/conf.d/wmp.conf',
+    ensure  => file,
+    content  => template("wmp-site/wmp.conf.erb"),
+  }
+
+}
