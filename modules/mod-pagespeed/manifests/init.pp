49770dbd9524bae839f9699dbb916da7f84d648f
root <root@puppet.aws.internal>
2013-03-21 17:58:30 +0000

    mod-pagespeed module

---
+++ modules/mod-pagespeed/manifests/init.pp
@@ -0,0 +1,15 @@
+# == Class: mod-pagespeed
+class mod-pagespeed {
+
+  package { 'mod-pagespeed-stable':
+    ensure  => installed,
+    require => Package['httpd'],
+  } ~>
+
+  file { 'pagespeed.conf':
+    path    => '/etc/httpd/conf.d/pagespeed.conf',
+    ensure  => file,
+    source  => "puppet:///modules/mod-pagespeed/pagespeed.conf",
+  }
+
+}
