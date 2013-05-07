7426e8737d0d526c3ef353bf76b5154b2e80bd68
root <root@puppet.aws.internal>
2013-05-07 10:55:39 +0000

    Remove mysql - my.cnf dependency to reduce risk of cluster restart/fail

--- modules/mysql-repl-wmp/manifests/init.pp
+++ modules/mysql-repl-wmp/manifests/init.pp
@@ -5,11 +5,14 @@ class mysql-repl-wmp {
     ensure  => installed
   } ~>
 
+# NB: Do not create a service dependency between my.cnf and mysqld
+# NB: If you do, all nodes will be restarted which may bring down the cluster
   file { 'my.cnf':
     path    => '/etc/my.cnf',
     ensure  => file,
     source => 'puppet:///modules/mysql-repl-wmp/my.cnf',
-  } ~>
+  }
+# NB: Seriously, don't do it
 
   service { 'mysql':
     name => 'mysql',
