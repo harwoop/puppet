af2b26e9b134eaed013e79354f9b7322ec84c13a
root <root@puppet.aws.internal>
2013-05-20 17:18:44 +0000

    Yum repos module

---
+++ modules/yum-repos/manifests/init.pp
@@ -0,0 +1,23 @@
+class yum-repos {
+
+  file { '/etc/yum.repos.d/10gen.repo':
+    ensure  => file,
+    source  => 'puppet:///modules/yum-repos/10gen.repo',
+  }
+
+  file { '/etc/yum.repos.d/cup.repo':
+    ensure  => file,
+    source  => 'puppet:///modules/yum-repos/cup.repo',
+  }
+
+  file { '/etc/yum.repos.d/mod-pagespeed.repo':
+    ensure  => file,
+    source  => 'puppet:///modules/yum-repos/mod-pagespeed.repo',
+  }
+
+  file { '/etc/yum.repos.d/newrelic.repo':
+    ensure  => file,
+    source  => 'puppet:///modules/yum-repos/newrelic.repo',
+  }
+
+}
