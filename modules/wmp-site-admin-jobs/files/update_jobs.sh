e2d70be4569a3e9229634bb65a10fa214775b091
root <root@puppet.aws.internal>
2014-04-15 15:43:14 +0000

    New order for admin jobs

--- modules/wmp-site-admin-jobs/files/update_jobs.sh
+++ modules/wmp-site-admin-jobs/files/update_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="http://localhost/tools/required/jobs?auth=5de4e738ecc1fe2bf2ba5120d45c956d&jID="
-JOBS="14 10 13 8 12 11 9 1 2 22"
+JOBS="14 10 9 13 8 12 11 1 2 22"
 LOG="/data/logs/httpd/wmp/admin_jobs.log"
 FILESDIR="/data/httpd/wmp/files/"
 
