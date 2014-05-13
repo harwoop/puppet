b67e9cc45aa01ae9f7e66f91b9468d220827184b
root <root@puppet.aws.internal>
2014-05-13 14:16:57 +0000

    add dynatrace agent

--- modules/dynatrace/manifests/init.pp
+++ modules/dynatrace/manifests/init.pp
@@ -1,16 +1,7 @@
-# == Class: dynatrace
-##????class php-wmp ($mysql_cache_enabled = true, $apc_cache_size = "128M") {
+# == Class: dynatrace-wsagent
+class dynatrace-wsagent {
 
-  package { 'dynatrace':
-#    ensure  => installed,
-#    require => Package['httpd'],
-#  }
-
-  file { 'dynatrace.conf':
-    path    => '/etc/httpd/conf.d/php.conf',
-    ensure  => file,
-    require => Package['dynatrace'],
-    source  => "puppet:///modules/dynatrace/dynatrace.conf",
-  }
-
-}
+  package { 'cup-dynatrace-wsagent':
+    ensure  => installed,
+    require => Package['httpd'],
+ }
