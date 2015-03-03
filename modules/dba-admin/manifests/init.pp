1058cf1fb8c1d3b87615e6e05e5a8859ae3a86da
root <root@puppet.aws.internal>
2015-03-03 15:34:41 +0000

    module to set up dba-admin user

---
+++ modules/dba-admin/manifests/init.pp
@@ -0,0 +1,45 @@
+class dba-admin {
+
+  group { 'dba-admingrp':
+    name => 'dba-admin',
+    gid => '571',
+    ensure => present,
+  }
+
+  user { 'dba-admin':
+    ensure => present,
+    uid => '570',
+    gid => 'dba-admin',
+    home => '/home/dba-admin',
+    require => Group['dba-admingrp'],
+  }
+
+  file { 'dba-admin-home':
+    path => '/home/dba-admin',
+    ensure => directory,
+    owner => 'dba-admin',
+    group => 'dba-admin',
+    mode => '0755',
+    require => User['dba-admin'],
+  }
+
+  file { 'dba-admin-ssh':
+    path => '/home/dba-admin/.ssh',
+    ensure => directory,
+    owner => 'dba-admin',
+    group => 'dba-admin',
+    mode => '0700',
+    require => File['dba-admin-home'],
+  }
+
+  file { 'dba-admin-ssh-key':
+    path => '/home/dba-admin/.ssh/authorized_keys',
+    ensure => file,
+    owner => 'dba-admin',
+    group => 'dba-admin',
+    mode => '0600',
+    source  => "puppet:///modules/dba-admin/authorized_keys",
+    require => File['dba-admin-ssh'],
+  }
+
+}
