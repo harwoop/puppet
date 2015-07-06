5941a48c9cb0492270010fee2adbe8056ca2e03d
root <root@puppet.aws.internal>
2015-07-06 12:18:49 +0000

    release 15.7 marvel ticket 156789

--- modules/wmp-site-admin-jobs/files/update_jobs.sh
+++ modules/wmp-site-admin-jobs/files/update_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 URL="http://localhost/tools/required/jobs?auth=5de4e738ecc1fe2bf2ba5120d45c956d&jID="
-JOBS="129 1 2 22"
+JOBS="129 1 2 22 108"
 LOG="/data/logs/httpd/wmp/admin_jobs.log"
 FILESDIR="/data/httpd/wmp/files/"
 echo "---- `date` ----" >> $LOG
