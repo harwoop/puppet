c8c8b373b2c18e89a5c06f32ffad9b37e7c0e9b3
Martin Jasiczak <mjasiczak@cambridge.org>
2016-02-17 15:14:34 +0000

    MI 202191 fixing pagespeed to restart httpd

--- modules/mod-pagespeed/manifests/init.pp
+++ modules/mod-pagespeed/manifests/init.pp
@@ -10,6 +10,7 @@ class mod-pagespeed {
     path    => '/etc/httpd/conf.d/pagespeed.conf',
     ensure  => file,
     source  => "puppet:///modules/mod-pagespeed/pagespeed.conf",
+    notify  => Service["httpd"],
   }
 
 }
