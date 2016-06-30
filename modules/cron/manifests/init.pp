46d1b3bdc60cf681275d0c113a1a41dd29baede6
Martin Jasiczak <mjasiczak@cambridge.org>
2016-06-30 13:11:27 +0000

    Modification to run script to manager Puppet clientbucket directories

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -2,6 +2,7 @@ class cron {
         $minute1 = generate('/usr/bin/env', 'sh', '-c',  'printf $((RANDOM%30+0))')
         $minute2 = $minute1 + 30
         $minute3 = $minute1 + 15
+	$minute4 = $minute1 + 18
 
         cron { "manual-puppet":
                 command => "/usr/bin/puppet agent --onetime --no-daemonize --logdest syslog > /dev/null 2>&1",
@@ -15,6 +16,21 @@ class cron {
 		ensure => stopped,
 		enable => false,
 	}
+
+        file { 'puppet_clientbucket_cleanup.sh':
+    		path   => '/usr/local/bin/puppet_clientbucket_cleanup.sh',
+    		ensure => present,
+    		mode   => 750,
+    		source => "puppet:///modules/cron/full_import.sh",
+  	} 
+
+        cron { "puppet-clear-log-dirs":
+                command => "/usr/local/bin/puppet_clientbucket_cleanup.sh >2&1",
+                user => "root",
+                hour => 5,
+                minute => $minute4,
+                ensure => present,
+        }
 
         cron { "puppet-clear-logs":
                 command => "/usr/bin/find /var/lib/puppet/clientbucket/ -mtime +14 -atime +14 -delete > /dev/null 2>&1",
